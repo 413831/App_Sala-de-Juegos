85fd21d77175059d2d2f22046e07c2eb31b5a528
413831 <alepleins3pp@gmail.com>
2020-04-04 15:53:26 -0300

    Primer commit

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
-import {Subscription} from "rxjs";
-import {TimerObservable} from "rxjs/observable/TimerObservable";
+import {Subscription, BehaviorSubject} from "rxjs";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,7 +13,7 @@ export class LoginComponent implements OnInit {
   usuario = '';
   clave= '';
   progreso: number;
-  progresoMensaje="esperando..."; 
+  progresoMensaje="esperando...";
   logeando=true;
   ProgresoDeAncho:string;
 
@@ -37,23 +36,24 @@ export class LoginComponent implements OnInit {
     }
   }
   MoverBarraDeProgreso() {
-    
+
     this.logeando=false;
     this.clase="progress-bar progress-bar-danger progress-bar-striped active";
-    this.progresoMensaje="NSA spy..."; 
-    let timer = TimerObservable.create(200, 50);
-    this.subscription = timer.subscribe(t => {
+    this.progresoMensaje="NSA spy...";
+    //let timer = TimerObservable.create(200, 50);
+    let bs = new BehaviorSubject<boolean>(false);//ver
+    this.subscription = bs.subscribe(t => {
       console.log("inicio");
       this.progreso=this.progreso+1;
       this.ProgresoDeAncho=this.progreso+20+"%";
       switch (this.progreso) {
         case 15:
         this.clase="progress-bar progress-bar-warning progress-bar-striped active";
-        this.progresoMensaje="Verificando ADN..."; 
+        this.progresoMensaje="Verificando ADN...";
           break;
         case 30:
           this.clase="progress-bar progress-bar-Info progress-bar-striped active";
-          this.progresoMensaje="Adjustando encriptación.."; 
+          this.progresoMensaje="Adjustando encriptación..";
           break;
           case 60:
           this.clase="progress-bar progress-bar-success progress-bar-striped active";
@@ -67,13 +67,13 @@ export class LoginComponent implements OnInit {
           this.clase="progress-bar progress-bar-success progress-bar-striped active";
           this.progresoMensaje="Instalando KeyLogger..";
           break;
-          
+
         case 100:
           console.log("final");
           this.subscription.unsubscribe();
           this.Entrar();
           break;
-      }     
+      }
     });
     //this.logeando=true;
   }
