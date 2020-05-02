c5a3b2656ee23349f62b248cfb42729ef0a3bb97
413831 <alepleins3pp@gmail.com>
2020-05-02 18:41:21 -0300

    Nuevo juego 02/05

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   database: 'https://tp-sala-de-juegos-labiv.firebaseio.com/',
   jugadores: 'archivoJugadores.json',
-  juegos: 'archivoJuegos.json'
+  juegos: 'archivoJuegos.json',
+  palabras: ['tomate','zanahoria','vehiculo','persona','episcopal','vasija','palindromo',
+          'elefante']
 };
